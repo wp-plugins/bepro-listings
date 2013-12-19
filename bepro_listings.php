801c4238a690e003c83653ee913001e4b58f20ae
BeyondPrograms <BeyondPrograms@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-19 13:00:40 +0000

    fix activation
    git-svn-id: https://plugins.svn.wordpress.org/bepro-listings/trunk@825533 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bepro_listings.php
+++ bepro_listings.php
@@ -363,7 +363,7 @@ class Bepro_listings{
 	function bepro_listings_activate() {
 		global $wpdb, $wp_rewrite;  
 		
-		$wp_rewrite->flush_rules();	
+		Bepro_listings::flush_rules();	
 		
 		if (function_exists('is_multisite') && is_multisite()){ 
 			$blogids = $wpdb->get_col($wpdb->prepare("SELECT blog_id FROM $wpdb->blogs"));
